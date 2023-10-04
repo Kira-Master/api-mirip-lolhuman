5ccb662f2fb88977365ba065b81c8eca3a8cb7b9
KIRA MASTER <47713860+Kira-Master@users.noreply.github.com>
2023-10-04 17:59:46 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,7 @@ var lolkill = db.get("lolkill");
 	console.log('')
 }
 
-var creator = 'ZeeoneOfc' // ubah jadi nama lu
+var creator = 'Zexxa Dev' // ubah jadi nama lu
 const listkey = ["Alphabot","Zeeone","ZeeoneOfc"]; // ubah apikey nya, tambah aja klo mau
 
 var axios = require('axios');
